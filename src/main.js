30cce3a37a73973ca0b96b0e8fcb9fcf6c0b9b44
moni-apps <47895257+moni-apps@users.noreply.github.com>
2019-02-23 00:51:33 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -16,11 +16,8 @@ var todoStorage = {
 new Vue({
   el: '#app',
   data: {
-    // ★STEP5 localStorage から 取得した ToDo のリスト
     todos: [],
-    // ★STEP11 抽出しているToDoの状態
     current: 0,
-    // ★STEP11＆STEP13 各状態のラベル
     options: [
       { value: -1, label: 'すべて', text: 'All', text2: 'fas fa-list-ul' },
       { value: 0, label: '作業中', text: 'List', text2: 'fas fa-tasks' },
@@ -29,66 +26,41 @@ new Vue({
   },
 
   computed: {
-
-    // ★STEP12
     computedTodos: function () {
       return this.todos.filter(function (el) {
         return this.current < 0 ? true : this.current === el.state
       }, this)
     },
-
-    // ★STEP13 作業中・完了のラベルを表示する
     labels() {
       return this.options.reduce(function (a, b) {
         return Object.assign(a, { [b.value]: b.label })
       }, {})
-      // キーから見つけやすいように、次のように加工したデータを作成
-      // {0: '作業中', 1: '完了', -1: 'すべて'}
     }
   },
-
-  // ★STEP8
   watch: {
-    // オプションを使う場合はオブジェクト形式にする
     todos: {
-      // 引数はウォッチしているプロパティの変更後の値
       handler: function (todos) {
         todoStorage.save(todos)
       },
-      // deep オプションでネストしているデータも監視できる
       deep: true
     }
   },
-
-  // ★STEP9
   created() {
-    // インスタンス作成時に自動的に fetch() する
     this.todos = todoStorage.fetch()
   },
-
   methods: {
-
-    // ★STEP7 ToDo 追加の処理
     doAdd: function(event, value) {
-      // ref で名前を付けておいた要素を参照
       var comment = this.$refs.comment
-      // 入力がなければ何もしないで return
       if (!comment.value.length) {
         return
       }
-      // { 新しいID, コメント, 作業状態 }
-      // というオブジェクトを現在の todos リストへ push
-      // 作業状態「state」はデフォルト「作業中=0」で作成
       this.todos.push({
         id: todoStorage.uid++,
         comment: comment.value,
         state: 0
       })
-      // フォーム要素を空にする
       comment.value = ''
     },
-
-    // ★STEP10 状態変更の処理
     doChangeState: function (item) {
       item.state = !item.state ? 1 : 0
       var sound = [
@@ -109,12 +81,9 @@ new Vue({
       audio.src = randSounds
       audio.play()
     },
-
-    // ★STEP10 削除の処理
     doRemove: function (item) {
       var index = this.todos.indexOf(item)
       this.todos.splice(index, 1)
     }
-
   }
 })
